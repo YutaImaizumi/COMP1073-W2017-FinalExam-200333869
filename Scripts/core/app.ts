1d85546b8b2af11917ad2919960d24950d18c36f
Yuta Imaizumi <atamatock@gmail.com>
2017-04-19 12:50:56 -0400

    Setting base layout

--- Scripts/core/app.ts
+++ Scripts/core/app.ts
@@ -1,3 +1,11 @@
+// This is my portional original JavaScript data (Final Exam on COMP1073). 
+// The Dice Roller
+// File name : app.ts
+// Author's name : Yuta Imaizumi #200333869
+// Web site name : Intro To CreateJS
+
+
+
 // IIFE - Immediately Invoked Function Expression
 (function () {
 
